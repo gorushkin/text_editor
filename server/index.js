7d07b5f4f6a137053d934e410b72f33833c962e2
gorushkin <gorushkin@gmail.com>
2021-05-10 23:28:10 +0300

    add img

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 // import bodyParser from 'body-parser';
+import fs from 'fs';
+import path from 'path';
 import morgan from 'morgan';
 import multiparty from 'connect-multiparty';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 console.log('start back');
 
-const MultipartyMiidleware = multiparty({ uploadDir: './images' });
+const MultipartyMiidleware = multiparty({ uploadDir: './server/images' });
 
 const app = express();
 
@@ -22,10 +28,22 @@ app.get('/test', (req, res) => {
   res.status(200).json({ message: "It's express" });
 });
 
+app.use(express.static('./uploads'));
+app.use(express.static('./server/uploads'));
+
 app.post('/uploads', MultipartyMiidleware, (req, res) => {
-  console.log(req.files.upload);
-  console.log('img');
   const tempFile = req.files.upload;
-})
+  const tempPathFile = tempFile.path;
+  const targetPathUrl = path.join(__dirname, './uploads', tempFile.name);
+  if (path.extname(tempFile.originalFilename.toLowerCase()) === '.png' || '.jpg') {
+    fs.rename(tempPathFile, targetPathUrl, (err) => {
+      res.status(200).json({
+        uploaded: true,
+        url: `${tempFile.originalFilename}`,
+      });
+      if (err) return console.log(err);
+    });
+  }
+});
 
 app.listen(PORT, console.log(`app started at port ${PORT}`));
